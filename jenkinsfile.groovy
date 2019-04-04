9c9d6df8aad14194d91de9c4b41fca43bf71d999
Konstantin Makarychev <mrsimpson@inbox.ru>
2019-04-04 14:26:20 +0300

    Add jenkinsfile and version

---
+++ jenkinsfile.groovy
@@ -0,0 +1,25 @@
+def repository = 'hydro-serving-manager'
+
+def buildFunction={
+    sh "sbt test"
+    sh "sbt docker"
+}
+
+def collectTestResults = {
+    junit testResults: '**/target/test-reports/io.hydrosphere*.xml', allowEmptyResults: true
+}
+
+pipelineCommon(
+        repository,
+        false, //needSonarQualityGate,
+        ["hydrosphere/serving-manager"],
+        collectTestResults,
+        buildFunction,
+        buildFunction,
+        buildFunction,
+        null,
+        "",
+        "",
+        {},
+        commitToCD("manager")
+)
