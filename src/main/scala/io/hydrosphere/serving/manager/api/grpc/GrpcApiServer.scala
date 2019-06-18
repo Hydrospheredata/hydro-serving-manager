365a3c20305b6e79564008d1c27475599bc50dc8
Bulat <lb6557@gmail.com>
2019-06-18 13:34:50 +0300

    add repos to GRPC

--- src/main/scala/io/hydrosphere/serving/manager/api/grpc/GrpcApiServer.scala
+++ src/main/scala/io/hydrosphere/serving/manager/api/grpc/GrpcApiServer.scala
@@ -13,17 +13,17 @@ import scala.concurrent.ExecutionContext
 
 object GrpcApiServer {
   def apply[F[_] : ConcurrentEffect](
-    managerRepositories: Repositories[F],
-    managerServices: Services[F],
-    managerConfiguration: ManagerConfiguration,
+    repos: Repositories[F],
+    services: Services[F],
+    config: ManagerConfiguration,
     appsHub: ApplicationSubscriber[F],
     servablesHub: ServableSubscriber[F]
   )(implicit ex: ExecutionContext): Server = {
 
-    val managerGrpcService = new ManagerGrpcService(managerRepositories.modelVersionRepository, managerServices.servableService)
-    val discoveryService = new GrpcServingDiscovery[F](appsHub, servablesHub)
+    val managerGrpcService = new ManagerGrpcService(repos.modelVersionRepository, services.servableService)
+    val discoveryService = new GrpcServingDiscovery[F](appsHub, servablesHub, repos.applicationRepository, repos.servableRepository)
 
-    val builder = BuilderWrapper(io.grpc.ServerBuilder.forPort(managerConfiguration.application.grpcPort))
+    val builder = BuilderWrapper(io.grpc.ServerBuilder.forPort(config.application.grpcPort))
       .addService(ManagerServiceGrpc.bindService(managerGrpcService, ex))
       .addService(ServingDiscoveryGrpc.bindService(discoveryService, ex))
 
