e4023222e039f912eca5226fd7296868a4e8f6a0
Bulat Lutfullin <lb6557@gmail.com>
2019-12-05 00:05:21 +0300

    add info about license and link to external docs

--- src/main/scala/io/hydrosphere/serving/manager/api/http/controller/SwaggerDocController.scala
+++ src/main/scala/io/hydrosphere/serving/manager/api/http/controller/SwaggerDocController.scala
@@ -3,7 +3,7 @@ package io.hydrosphere.serving.manager.api.http.controller
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import com.github.swagger.akka.SwaggerHttpService
-import com.github.swagger.akka.model.Info
+import com.github.swagger.akka.model.{Info, License}
 import io.swagger.models.{ExternalDocs, Scheme}
 
 class SwaggerDocController(
@@ -15,7 +15,11 @@ class SwaggerDocController(
 ) extends SwaggerHttpService  {
   override val apiDocsPath = "docs"
 //  override def basePath: String = "/api/v2"  // TODO need to shorten controller URLs, so swagger-ui can shorten links
-  override val info = Info(version = version, title = "Hydroserving Manager service")
-  override val externalDocs = Some(new ExternalDocs("Github repository", "https://github.com/Hydrospheredata/hydro-serving"))
+  override val info = Info(
+    version = version,
+    title = "Hydroserving Manager service",
+    license = Some(License("Apache License 2.0", "https://github.com/Hydrospheredata/hydro-serving/blob/master/LICENSE"))
+  )
+  override val externalDocs = Some(new ExternalDocs("Find out more about Hydroserving", "https://hydrosphere.io/serving-docs"))
   override def schemes: List[Scheme] = Scheme.HTTPS :: Scheme.HTTP :: Nil
 }
