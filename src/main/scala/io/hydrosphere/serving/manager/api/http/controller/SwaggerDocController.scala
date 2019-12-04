315b8c88e5488c17bbc74d7289ccc69aa8bdf1f3
Bulat Lutfullin <lb6557@gmail.com>
2019-12-04 23:45:50 +0300

    additional info for swagger generator and fix for swagger index page

--- src/main/scala/io/hydrosphere/serving/manager/api/http/controller/SwaggerDocController.scala
+++ src/main/scala/io/hydrosphere/serving/manager/api/http/controller/SwaggerDocController.scala
@@ -14,7 +14,8 @@ class SwaggerDocController(
   implicit val materializer: ActorMaterializer
 ) extends SwaggerHttpService  {
   override val apiDocsPath = "docs"
-  override val info = Info(version = version)
-  override val externalDocs = Some(new ExternalDocs("ML Lambda", "https://github.com/Hydrospheredata/hydro-serving"))
+//  override def basePath: String = "/api/v2"  // TODO need to shorten controller URLs, so swagger-ui can shorten links
+  override val info = Info(version = version, title = "Hydroserving Manager service")
+  override val externalDocs = Some(new ExternalDocs("Github repository", "https://github.com/Hydrospheredata/hydro-serving"))
   override def schemes: List[Scheme] = Scheme.HTTPS :: Scheme.HTTP :: Nil
 }
