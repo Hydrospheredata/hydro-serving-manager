bc95fd9352fc58e526ec9b6273cda7d9790fe72f
Bulat Lutfullin <blutfullin@hydrosphere.io>
2020-06-09 14:35:47 +0300

    Bump version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val postgresqlVersion = "42.1.4"
   val scalaTestVersion = "3.0.3"
   val awsSdkVersion = "1.11.312"
-  val servingGrpcScala = "2.2.0"
+  val servingGrpcScala = "2.3.0"
   val catsV = "1.2.0"
   val envoyDataPlaneApi = "v1.6.0_1"
 
@@ -21,9 +21,7 @@ object Dependencies {
     "com.amazonaws" % "aws-java-sdk-route53" % awsSdkVersion
   )
 
-  lazy val kubernetesDependencies = Seq(
-    "io.skuber" %% "skuber" % "2.1.0"
-  )
+  lazy val kubernetesDependencies = Seq("io.skuber" %% "skuber" % "2.1.0")
 
   lazy val akkaDependencies = Seq(
     "com.typesafe.akka" %% "akka-actor" % akkaVersion,
@@ -35,7 +33,7 @@ object Dependencies {
     "com.typesafe.akka" %% "akka-http-core" % akkaHttpVersion,
     "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
     "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion,
-    "com.github.swagger-akka-http" %% "swagger-akka-http" % "1.0.0" exclude("javax.ws.rs", "jsr311-api"),
+    "com.github.swagger-akka-http" %% "swagger-akka-http" % "1.0.0" exclude ("javax.ws.rs", "jsr311-api"),
     "ch.megard" %% "akka-http-cors" % "0.2.1"
   )
 
@@ -43,7 +41,7 @@ object Dependencies {
     "com.thesamet.scalapb" %% "scalapb-runtime-grpc" % scalapb.compiler.Version.scalapbVersion,
     "io.hydrosphere" %% "serving-grpc-scala" % servingGrpcScala,
     "io.grpc" % "grpc-netty" % scalapb.compiler.Version.grpcJavaVersion
-  ).map(_ exclude("com.google.api.grpc", "proto-google-common-protos"))
+  ).map(_ exclude ("com.google.api.grpc", "proto-google-common-protos"))
 
   lazy val testDependencies = Seq(
     "org.mockito" % "mockito-all" % "1.10.19" % "test,it",
@@ -79,13 +77,13 @@ object Dependencies {
     Seq(
       "org.typelevel" %% "cats-effect" % catsV,
       "org.tpolecat" %% "doobie-core" % "0.7.0",
-      "org.tpolecat" %% "doobie-postgres"  % "0.7.0",
-      "com.zaxxer" % "HikariCP" % "2.6.3",  // doobie-hikari depends on hikari 3.3.1 which has weird pool retries.
+      "org.tpolecat" %% "doobie-postgres" % "0.7.0",
+      "com.zaxxer" % "HikariCP" % "2.6.3", // doobie-hikari depends on hikari 3.3.1 which has weird pool retries.
       "org.tpolecat" %% "doobie-scalatest" % "0.7.0" % "test, it",
       "io.hydrosphere" %% "typed-sql" % "0.1.0",
       "org.postgresql" % "postgresql" % postgresqlVersion,
       "org.flywaydb" % "flyway-core" % "4.2.0",
-      "com.spotify" % "docker-client" % "8.16.0" exclude("ch.qos.logback", "logback-classic"),
+      "com.spotify" % "docker-client" % "8.16.0" exclude ("ch.qos.logback", "logback-classic"),
       "com.google.guava" % "guava" % "22.0",
       "com.github.pureconfig" %% "pureconfig" % "0.9.1",
       "co.fs2" %% "fs2-core" % "1.0.5",
