241f31476a9133454a58142d4acce930c9b5bd56
techkuz <k.leshakov@yandex.ru>
2020-08-24 13:59:14 +0300

    Del redundant defaultMC value

--- src/main/scala/io/hydrosphere/serving/manager/domain/monitoring/MonitoringConfiguration.scala
+++ src/main/scala/io/hydrosphere/serving/manager/domain/monitoring/MonitoringConfiguration.scala
@@ -6,7 +6,5 @@ import DefaultJsonProtocol._
 case class MonitoringConfiguration(batchSize: Int = 100)
 
 object MonitoringConfiguration {
-  val defaultBatchSize: Int = 100
-
   implicit val JSONFormat: RootJsonFormat[MonitoringConfiguration] = jsonFormat1(MonitoringConfiguration.apply)
 }
