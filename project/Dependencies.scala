325349862ec4f40e0036ce6026d1bad63f04025e
techkuz <k.leshakov@yandex.ru>
2020-09-08 14:35:04 +0300

    Bump serving-grpc

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val postgresqlVersion = "42.2.4"
   val scalaTestVersion = "3.0.3"
   val awsSdkVersion = "1.11.312"
-  val servingGrpcScala = "2.3.1-dev3"
+  val servingGrpcScala = "2.4.0-rc2"
   val catsV = "1.2.0"
   val envoyDataPlaneApi = "v1.6.0_1"
 
