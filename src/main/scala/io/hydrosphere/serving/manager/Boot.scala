9da2efc70effd1d69ac6dfd00143270b3cb11e28
Bulat Lutfullin <blutfullin@provectus.com>
2021-02-09 13:04:10 +0300

    Add configuration parsing. Remove unused configurations.
    Signed-off-by: Bulat Lutfullin <blutfullin@provectus.com>

--- src/main/scala/io/hydrosphere/serving/manager/Boot.scala
+++ src/main/scala/io/hydrosphere/serving/manager/Boot.scala
@@ -9,15 +9,16 @@ import io.hydrosphere.serving.manager.util.ReflectionUtils
 import org.apache.logging.log4j.scala.Logging
 
 object Boot extends IOApp with Logging {
-  override def run(args: List[String]): IO[ExitCode] = IO.suspend {
-    for {
-      configuration <- ManagerConfiguration.load[IO]
-      _ <- IO(logger.info(s"Config loaded:\n${ReflectionUtils.prettyPrint(configuration)}"))
-      dockerClient <- IO(DefaultDockerClient.fromEnv().readTimeoutMillis(60 * 60 * 1000).build())
-      wrappedClient <- DockerdClient.create[IO](dockerClient)
-      _ <- App.make[IO](configuration, wrappedClient).use { app =>
-        app.httpServer.start() >> app.grpcServer.start() >> IO.never
-      }
-    } yield ExitCode.Success
-  }
+  override def run(args: List[String]): IO[ExitCode] =
+    IO.suspend {
+      for {
+        configuration <- ManagerConfiguration.load[IO]()
+        _             <- IO(logger.info(s"Config loaded:\n${ReflectionUtils.prettyPrint(configuration)}"))
+        dockerClient  <- IO(DefaultDockerClient.fromEnv().readTimeoutMillis(60 * 60 * 1000).build())
+        wrappedClient <- DockerdClient.create[IO](dockerClient)
+        _ <- App.make[IO](configuration, wrappedClient).use { app =>
+          app.httpServer.start() >> app.grpcServer.start() >> IO.never
+        }
+      } yield ExitCode.Success
+    }
 }
--- src/main/scala/io/hydrosphere/serving/manager/config/ManagerConfiguration.scala
+++ src/main/scala/io/hydrosphere/serving/manager/config/ManagerConfiguration.scala
@@ -1,32 +1,42 @@
 package io.hydrosphere.serving.manager.config
 
 import java.nio.file.Path
-
 import cats.effect.Sync
 import cats.syntax.either._
 import com.amazonaws.regions.Regions
-import pureconfig.ConfigReader
+import io.hydrosphere.serving.manager.domain.deploy_config.DeploymentConfiguration
+import pureconfig.{ConfigReader, ConfigSource}
 import pureconfig.error.CannotConvert
 import pureconfig.generic.auto._
+import io.circe.parser._
 
 case class ManagerConfiguration(
-  application: ApplicationConfig,
-  manager: AdvertisedConfiguration,
-  localStorage: Option[Path],
-  database: HikariConfiguration,
-  cloudDriver: CloudDriverConfiguration,
-  openTracing: OpenTracingConfiguration,
-  dockerRepository: DockerRepositoryConfiguration,
-  metrics: MetricsConfiguration
+    application: ApplicationConfig,
+    localStorage: Option[Path],
+    database: HikariConfiguration,
+    cloudDriver: CloudDriverConfiguration,
+    dockerRepository: DockerRepositoryConfiguration,
+    defaultDeploymentConfiguration: Option[DeploymentConfiguration]
 )
 
 object ManagerConfiguration {
-  implicit val regionsConfigReader = ConfigReader.fromString { str =>
-    Either.catchNonFatal(Regions.fromName(str))
+  implicit val depConfigReader: ConfigReader[DeploymentConfiguration] = ConfigReader.fromString {
+    str =>
+      decode[DeploymentConfiguration](str).leftMap { err =>
+        CannotConvert(str, classOf[DeploymentConfiguration].getSimpleName, err.getMessage)
+      }
+  }
+
+  implicit val regionsConfigReader: ConfigReader[Regions] = ConfigReader.fromString { str =>
+    Either
+      .catchNonFatal(Regions.fromName(str))
       .leftMap(e => CannotConvert(str, "Region", e.getMessage))
   }
 
-  def load[F[_]](implicit F: Sync[F]): F[ManagerConfiguration] = F.delay {
-    pureconfig.loadConfigOrThrow[ManagerConfiguration]
-  }
+  def load[F[_]](
+      configSource: ConfigSource = ConfigSource.default
+  )(implicit F: Sync[F]): F[ManagerConfiguration] =
+    F.delay {
+      configSource.loadOrThrow[ManagerConfiguration]
+    }
 }
